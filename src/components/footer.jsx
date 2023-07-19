9200b23cf224223704abc28a1bf91ee7bb3ca946
PERZI0N <vaidik.chhirolya1@gmail.com>
2023-07-19 10:37:14 +0530

    Completed the website with remaining pages.

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.css"
+function Footer(){
+    return (
+    <div className="foot">
+        <h3 className="me">Created by Vaidik Chhirolya</h3>
+    </div>
+    )
+}
+export default Footer;
